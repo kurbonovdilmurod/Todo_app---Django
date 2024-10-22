bbce7f8e863976365e2b6486b5a0d41eeda2db05
kurbonovdilmurod <dilmurodkurbonov653@gmail.com>
2024-10-22 22:45:27 +0500

    added core app and extended todo app

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from todo import views
 
 urlpatterns = [
-    path('', views.todos, name='todos'),
-    path('<int:pk>/', views.todo, name='todo'),
+    path('', views.add_todo, name='add_todo'),
+    path('<int:pk>/', views.view_todo, name='view_todo'),
+    path('edit/<pk>', views.edit_todo, name='edit_todo'),
+    path('delete/<pk>', views.delete_todo, name="delete_todo"),
+    path('cross_off/<pk>', views.cross_off_todo, name="cross_off_todo"),
+    path('uncross/<pk>', views.uncross_todo, name="uncross_todo"),
+
 ]
